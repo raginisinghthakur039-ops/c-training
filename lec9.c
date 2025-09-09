f95c0342e18d263cc6e8ed608205508427cfdfd3
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-09-09 15:48:50 +0530

    Add files via upload

---
+++ lec9.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+int main(){
+   /* int num=1;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<=i;j++){
+            printf("%d" ,num++);
+        }
+        printf("\n");
+    
+    }*/
+   /*int num=65;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<=i;j++){
+            printf("%c " ,num++);
+        }
+        printf("\n");
+    
+    }*/
+   /*int num =65;
+    for(int i=0;i<4;i++){
+        for(int j=3;j>i;j--){
+            printf(" ");
+        }
+        for(int k=0;k<=i;k++){
+            printf("%c",num++);
+        }
+        for(int p=1;p<=i;p++){
+            printf("%c",num++);
+        }
+
+        printf("\n");
+    
+    }*/
+    /*int num =65;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=3;k>i;k--){
+            printf("%c",num++);
+        }
+        for(int p=3;p>=i;p--){
+            printf("%c",num++);
+        }
+
+        printf("\n");
+    */
+  /* int num =65;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=i;j++){
+                 printf(" ");
+        }
+        for(int k=3;k>i;k--){
+            printf("*");https://github.com/raginisinghthakur039-ops/all
+        }
+        for(int p=3;p>=i;p--){
+            printf("*");
+        }
+
+        printf("\n");
+    
+    }*/
+   int num =65;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=3;k>i;k--){
+            printf("*");
+        }
+        for(int p=3;p>=i;p--){
+            printf("*");
+        }
+
+        printf("\n");
+    
+    }
+    }  
