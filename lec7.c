b0411cc4082d9bb8b5f4331a410d972265d75f3b
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-09-15 21:22:50 +0530

    lec7.c
    commit -m "lec7"

---
+++ lec7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n=8;
+    int t1=0;
+    int2=1;
+    int next;
+    for(int i=1;i<=n;i++){
+        printf("%d",t1);
+        next=t1+t2;
+        t1=t2;
+        t2=next;
+    }
+    return 0;
+}
