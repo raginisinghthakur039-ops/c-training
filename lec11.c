9655786eef0db69f5ebac7d793250764bda8f0b9
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-09-15 21:25:13 +0530

    lec11
    commit -m "lec11"

---
+++ lec11.c
@@ -0,0 +1,70 @@
+/*#include<stdio.h>
+int main(){
+    for(int i=0;i<=4;i++){
+        for(int j=0;j<=4;j++){
+            if(i==j||j==(4-i)){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+
+        }printf("\n");
+    }
+}*/
+/*#include<stdio.h>
+int sum(){
+    int a=10;
+    int b=12;
+    int c;
+    c=a+b;
+    printf("%d",c);
+    return 0;
+}
+int main(){
+    sum();
+}*/
+/*#include<stdio.h>
+int sum(int a,int b){
+    printf("%d",a+b);
+}
+int main(){
+    sum(5,10);
+    return 0;
+}*/
+// #include<stdio.h>
+
+// int odd_even(){
+//     int a;
+//     printf("Enter a number:");
+//     scanf("%d",&a);
+//     if(a%2==0){
+//         printf("even\n",&a);
+//     }else{
+//         printf("odd\n",&a);
+//     }
+// }
+// int main(){
+    
+//     odd_even();
+
+//     return 0;
+
+// }
+#include<stdio.h>
+int sum(){
+    int a=1;
+    int b=2;
+    int c=3;
+    int d=4;
+    int e;
+    e=a+b+c+d;
+    printf("%d",e);
+    return 0;
+}
+int main(){
+    sum();
+}
+    
+
+
+
