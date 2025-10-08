84e36a2bbf4d60a45441b90765a8246dfb4a4c36
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-10-08 21:43:15 +0530

    pattern
    pattern

---
+++ lec3.c
@@ -0,0 +1,95 @@
+// #include <stdio.h>
+
+// int main(){
+
+
+//     int a=76;
+//     int m=78;
+//     if(a>75 && m>75  ){
+//         printf("A");
+//     }else if(a>75 && m>65 && m<75){
+//         printf("B");
+// #include <stdio.h>
+// int main(){
+
+//     }else if(a>75  && m<65 && m>50 ){
+//         printf("C");
+//     }else if (a>75 && m<50 && m>33)
+//     int a ;
+//     scanf("%d",&a);
+//     switch (a)
+//     {
+//             printf("D");
+//     }
+    
+//     else{
+//           printf("aapka kuch nhi sakta");
+//     }
+    
+//     // int a=11;   /
+//     // int  b=21;
+
+//     //  if(a%2==0 ){
+//     //     printf("even");
+
+//     //  }else{
+//     //     printf("odd");
+//     case 1:
+//         printf("hello");
+//         break;
+
+//     //  }
+// //     int a=5;
+// //     int b=3;
+// //    int sum=a++ + ++b + --b + b-- + ++a;
+
+// //    printf("%d",sum);
+    
+//     // printf("%d",a);
+//     case 2:
+//         printf("hiii");
+//     default:
+//     printf("heheh");
+//         break;
+
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+int main(){
+
+    int a=5;
+    // (a<5)?printf("hello"):printf("hiii");
+    // int a;
+    // int b;
+    // char ch;
+    // scanf("%d %d %c", &a, &b, &ch);
+    // printf("%d %d %c", a, b, ch);
+    // if(ch=='+'){
+
+    // }
+    // int a=5;
+    // int m ;
+    // if(a==5)
+    //   printf("hehehe")
+    //   printf("hehehe")
+
+    // scanf("%d %d",&a,&m);
+    // if(a>75 && m >75){
+    //     printf("A");
+    // }else if(a>75 && m<75 && m>65){
+    //     printf("B");
+
+    // }
+    // if(a==5 || a>4){
+    //     printf("hello");
+
+    // }else
+    // {
+    //     printf("byee");
+       
+    // }
+    
+    // printf("hehehe");
+}
